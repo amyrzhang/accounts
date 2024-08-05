33cc25ff85842b3247b20ca76d5e6f39f0d6a3b4
Rui Zhang <641619594@qq.com>
2024-08-05 23:15:45 +0800

    增加检查账单模块

--- solution.py
+++ solution.py
@@ -3,7 +3,7 @@ import pandas as pd
 
 def strip_in_data(data):  # 把列名中和数据中首尾的空格都去掉。
     data = data.rename(columns={column_name: column_name.strip() for column_name in data.columns})
-    data = data.applymap(lambda x: x.strip().strip('¥') if isinstance(x, str) else x)
+    data = data.map(lambda x: x.strip().strip('¥') if isinstance(x, str) else x)
     return data
 
 
@@ -33,8 +33,17 @@ def read_data_zfb(path):  # 获取支付宝数据
     d_zfb.insert(4, '类型', "商户消费", allow_duplicates=True)  # 添加类型标识
     len2 = len(d_zfb)
     print("成功读取 " + str(len2) + " 条「支付宝」账单数据\n")
-    return d_zfb
+    return process_data_zfb(d_zfb)
 
+
+def process_data_zfb(data_zfb):
+    return data_zfb[(data_zfb['支付状态'] == '交易成功') & (data_zfb['收/支'] != '不计收支')]
+
+def read_data_bank(path):
+    data = pd.read_csv(path)
+    data['交易时间'] = pd.to_datetime(data['交易时间'])
+    data['金额'] = data['金额'].astype('float64')
+    return data
 
 def add_cols(data):  # 增加3列数据
     # 逻辑1：取值-1 or 1。-1表示支出，1表示收入。
@@ -61,7 +70,8 @@ def add_cols(data):  # 增加3列数据
     data.insert(9, '逻辑2', 1, allow_duplicates=True)  # 插入列，默认值为1
     for index in range(len(data.iloc[:, 3])):  # 遍历第4列的值，判断为资金流动，则改'逻辑2'为0
         col3 = data.iloc[index, 3]
-        if (col3 == '提现已到账') or (col3 == '已全额退款') or (col3 == '已退款') or (col3 == '退款成功') or (col3 == '还款成功') or (
+        if (col3 == '提现已到账') or (col3 == '已全额退款') or (col3 == '已退款') or (col3 == '退款成功') or (
+                col3 == '还款成功') or (
                 col3 == '交易关闭'):
             data.iloc[index, 9] = 0
 
@@ -77,3 +87,22 @@ def add_cols(data):  # 增加3列数据
         money = data.iloc[index, 8] * data.iloc[index, 9] * data.iloc[index, 10]
         data.iloc[index, 11] = money
     return data
+
+
+def check_bill_data(bill_data):
+    data_resource = bill_data['来源'].unique()
+    min_date, max_date = str(min(bill_data['交易时间']))[:10], str(max(bill_data['交易时间']))[:10]
+    bill_date_range = min_date + '至' + max_date
+    bill_num = bill_data.shape[0]
+    bill_summary = bill_data.groupby('收/支')['金额'].sum()
+    summary = """
+    账单来源：{}
+    账单周期：{}
+    账单数量：{}条
+    支出金额：￥{}
+    收入金额：￥{}
+    结余金额：￥{}
+    """.format(data_resource, bill_date_range, bill_num, bill_summary['支出'], bill_summary['收入'],bill_summary['收入']-bill_summary['支出'],)
+    print(summary)
+
+    return summary
