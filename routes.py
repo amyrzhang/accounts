4747078cdbf89d37e8a2da4ebc541143ba6d3825
Rui Zhang <641619594@qq.com>
2024-10-19 18:19:21 +0800

    增加Bulueprint操作数据库模型

--- routes.py
+++ routes.py
@@ -1,10 +1,82 @@
 # -*- coding: utf-8 -*-
 # 定义API路由
-from flask import Blueprint, request, jsonify
-from models import db, Transaction
+from flask import Flask, Blueprint, request, jsonify
+from flask_cors import CORS
 from datetime import datetime
+import os
+
+from models import db, Transaction
+from query import Analyzer
+from uploader import write_db
+from utils import format_currency, format_percentage
 
 api = Blueprint('api', __name__)
+app = Flask(__name__)
+app.config['UPLOAD_FOLDER'] = 'uploads'
+os.makedirs(app.config['UPLOAD_FOLDER'], exist_ok=True)
+CORS(app)  # 允许所有来源
+
+
+@app.route('/api/data', methods=['GET'])
+def get_transactions():
+    a = Analyzer()
+    a.rename()
+    if request.args:
+        a.filter(params=request.args)
+    return jsonify(a.df.to_dict(orient='records'))
+
+
+@app.route('/api/report', methods=['GET'])
+def get_monthly_report():
+    a = Analyzer()
+    a.filter_monthly()
+    return jsonify({
+        'expenditure': -a.sums['支出'],
+        'income': a.sums['收入'],
+        'balance': a.sums.sum()
+    })
+
+
+@app.route('/api/report/category', methods=['GET'])
+def get_category_report():
+    a = Analyzer()
+    a.filter_monthly()
+    return jsonify(a.category_sums['支出'].abs().sort_values(ascending=False).to_dict())
+
+
+@app.route('/api/report/account', methods=['GET'])
+def get_account_report():
+    a = Analyzer()
+    a.filter_monthly()
+    res = a.account_sums
+    return jsonify(res.reset_index().to_dict(orient='records'))
+
+
+@app.route('/api/report/top10', methods=['GET'])
+def get_top10_transactions():
+    a = Analyzer()
+    a.filter_monthly()
+    res = a.top10_transactions
+    res['金额'] = res['金额'].apply(format_currency)
+    res['cdf'] = res['cdf'].apply(format_percentage)
+    return jsonify(res.to_dict(orient='records'))
+
+
+@app.route('/upload', methods=['POST'])
+def upload_file():
+    if 'file' not in request.files:
+        return "No file part", 400
+    file = request.files['file']
+    if file.filename == '':
+        return "No selected file", 400
+    if file:
+        file_name = file.filename
+        file_path = os.path.join(app.config['UPLOAD_FOLDER'], file_name)
+        if os.path.exists(file_path):
+            return f"File already exists: {file_name}", 409
+        file.save(file_path)
+        write_db(file_path)
+        return f"File saved successfully as {file_name}", 200
 
 
 @api.route('/transactions', methods=['POST'])
@@ -65,3 +137,9 @@ def delete_transaction(id):
     db.session.delete(transaction)
     db.session.commit()
     return '', 204
+
+
+if __name__ == '__main__':
+    with app.app_context():
+        db.create_all()
+    app.run(debug=True, host='0.0.0.0', port=18080)
