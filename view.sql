c0f2a49afe6e9708d7b27069e377414b5ef7942f
Rui Zhang <641619594@qq.com>
2024-11-13 17:37:35 +0800

    新增卡余额视图

--- view.sql
+++ view.sql
@@ -1,3 +1,7 @@
+# 历史余额
+# INSERT INTO transaction (time, source, expenditure_income, counterparty, pay_method, amount, category, goods)
+# VALUES ('2024-06-30 23:59:59', '手工', '收入', '', '民生银行储蓄卡(4827)',  675.72, '历史余额', '历史余额');
+
 # 月度收支
 create view monthly_balance as
 select month, income - expenditure as balance, income, expenditure
@@ -10,61 +14,57 @@ order by month desc;
 
 
 # 卡余额
-# drop view card_balance;
-# create view card_balance as
-with transfer_tb as (select time
-                          , expenditure_income
-                          , counterparty
-                          , pay_method
-                          , amount
+create view card_balance as
+with transfer_tb as (select *
                      from money_track.transaction
                      where pay_method regexp '^.{2}银行储蓄卡\\([0-9]{4}\\)$' # 用收付款账户都是银行卡判断【转账】
                        and counterparty regexp '^.{2}银行储蓄卡\\([0-9]{4}\\)$'),
-     spending_tb as (select time
-                          , expenditure_income
-                          , counterparty
-                          , pay_method
-                          , amount
+     spending_tb as (select *
                      from money_track.transaction
                      where not (pay_method regexp '^.{2}银行储蓄卡\\([0-9]{4}\\)$'
-                         and counterparty regexp '^.{2}银行储蓄卡\\([0-9]{4}\\)$')),
-     transaction_tb as (select time, expenditure_income, counterparty, pay_method, amount
+                         and counterparty regexp '^.{2}银行储蓄卡\\([0-9]{4}\\)$')), # 空值无法匹配
+     transaction_tb as (select *
                         from spending_tb
                         union all
-                        select
-                            time
+                        select id
+                             , time
+                             , source
                              , '支出' as expenditure_income
+                             , status
+                             , type
+                             , category
                              , counterparty
-                             , pay_method
+                             , goods
+                             , reversed
                              , amount
+                             , pay_method
+                             , processed_amount
                         from transfer_tb
                         union all
-                        select
-                            time
-                             , '收入' as expenditure_income
-                             , pay_method as counterparty
-                             , counterparty as pay_method
+                        select id
+                             , time
+                             , source
+                             , '收入'       as expenditure_income
+                             , status
+                             , type
+                             , category
+                             , pay_method   as counterparty
+                             , goods
+                             , reversed
                              , amount
+                             , counterparty as pay_method
+                             , processed_amount
                         from transfer_tb)
-
-select date_format(time, '%Y-%m')                      as month
-     , pay_method                                      as card
-     , sum(case
+select *,
+       sum(case
                when expenditure_income = '收入' then amount
                when expenditure_income = '支出' then -amount
-               else 0 end)                             as balance
-     , sum(if(expenditure_income = '收入', amount, 0)) as income
-     , sum(if(expenditure_income = '支出', amount, 0)) as expenditure
-from transaction_tb
-group by month, pay_method
-order by month desc, pay_method desc;
+               else 0 end) over (partition by pay_method order by time asc) as balance
+from transaction_tb;
 
 
-select *
-from transaction
-where amount >=1000;
 
-
+# 校准不计收支账单
 select date_format(time, '%Y-%m') as month,
        sum(case when expenditure_income = '收入' then amount
                 when expenditure_income = '支出' then -amount
@@ -93,7 +93,17 @@ where amount >= 1000;
 # order by month desc, pay_method desc;
 
 
-
+select-- date_format(time, '%Y-%m')                      as month
+      pay_method                                      as card
+     , sum(case
+               when expenditure_income = '收入' then amount
+               when expenditure_income = '支出' then -amount
+               else 0 end)                             as balance
+     , sum(if(expenditure_income = '收入', amount, 0)) as income
+     , sum(if(expenditure_income = '支出', amount, 0)) as expenditure
+from card_balance
+group by  pay_method
+order by  pay_method desc;
 
 
 
