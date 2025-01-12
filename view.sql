5a12dab3296363a6fc25abaf997114edb1b84dc7
Rui Zhang <641619594@qq.com>
2025-01-12 09:39:46 +0800

    修改卡余额视图

--- view.sql
+++ view.sql
@@ -1,55 +1,56 @@
 # 收支记录表
-CREATE TABLE `transaction` (
-  `id` int NOT NULL AUTO_INCREMENT,
-  `time` datetime NOT NULL,
-  `source` varchar(128) NOT NULL,
-  `expenditure_income` varchar(10) NOT NULL,
-  `status` varchar(10) DEFAULT NULL,
-  `type` varchar(10) DEFAULT NULL,
-  `category` varchar(128) NOT NULL,
-  `counterparty` varchar(128) DEFAULT NULL,
-  `goods` varchar(128) NOT NULL,
-  `reversed` tinyint(1) NOT NULL DEFAULT '0',
-  `amount` decimal(10,2) NOT NULL,
-  `pay_method` varchar(20) NOT NULL,
-  `processed_amount` decimal(10,2) DEFAULT '0.00',
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=388 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+CREATE TABLE `transaction`
+(
+    `id`                 int            NOT NULL AUTO_INCREMENT,
+    `time`               datetime       NOT NULL,
+    `source`             varchar(128)   NOT NULL,
+    `expenditure_income` varchar(10)    NOT NULL,
+    `status`             varchar(10)             DEFAULT NULL,
+    `type`               varchar(10)             DEFAULT NULL,
+    `category`           varchar(128)   NOT NULL,
+    `counterparty`       varchar(128)            DEFAULT NULL,
+    `goods`              varchar(128)   NOT NULL,
+    `reversed`           tinyint(1)     NOT NULL DEFAULT '0',
+    `amount`             decimal(10, 2) NOT NULL,
+    `pay_method`         varchar(20)    NOT NULL,
+    `processed_amount`   decimal(10, 2)          DEFAULT '0.00',
+    PRIMARY KEY (`id`)
+) ENGINE = InnoDB
+  AUTO_INCREMENT = 388
+  DEFAULT CHARSET = utf8mb4
+  COLLATE = utf8mb4_0900_ai_ci;
 
 # 历史余额
 # INSERT INTO transaction (time, source, expenditure_income, counterparty, pay_method, amount, category, goods)
 # VALUES ('2024-06-30 21:28:17', '手工', '收入', '', '民生银行储蓄卡(4827)',  675.72, '历史余额', '历史余额');
 
 # 资产信息表
-CREATE TABLE `asset_info` (
-  `id` int NOT NULL AUTO_INCREMENT,
-  `asset_name` varchar(255) NOT NULL,
-  `asset_type` varchar(50) NOT NULL,
-  `is_active` tinyint(1) NOT NULL DEFAULT '1',
-  `is_included` tinyint(1) NOT NULL DEFAULT '1',
-  `create_time` datetime DEFAULT CURRENT_TIMESTAMP,
-  `update_time` datetime DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=10 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+CREATE TABLE `account_info`
+(
+    `id`           int          NOT NULL AUTO_INCREMENT,
+    `account_name` varchar(255) NOT NULL,
+    `account_type` varchar(50)  NOT NULL,
+    `is_active`    tinyint(1)   NOT NULL DEFAULT '1',
+    `is_included`  tinyint(1)   NOT NULL DEFAULT '1',
+    `create_time`  datetime              DEFAULT CURRENT_TIMESTAMP,
+    `update_time`  datetime              DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+    PRIMARY KEY (`id`)
+) ENGINE = InnoDB
+  AUTO_INCREMENT = 10
+  DEFAULT CHARSET = utf8mb4
+  COLLATE = utf8mb4_0900_ai_ci;
 
-
-
-
-
-
-SELECT
-    a.asset_name
-     , asset_type
+SELECT a.account_name
+     , account_type
      , t.balance
      , t.balance as total
-FROM asset_info a
+FROM account_info a
          left join (SELECT *,
                            ROW_NUMBER() OVER (PARTITION BY pay_method ORDER BY time DESC) AS rn
-                    FROM card_transaction) t on a.asset_name = t.pay_method
+                    FROM card_transaction) t on a.account_name = t.pay_method
 WHERE a.is_active = 1
   and a.is_included = 1
   and t.rn = 1;
-
 
 
 
@@ -102,11 +103,87 @@ select *,
                else 0 end) over (partition by pay_method order by time asc) as balance
 from transaction_tb;
 
+# 卡余额，用账户关联
+drop view card_balance;
+create view card_balance as
+with transfer_tb as (select t.id,
+                            t.time,
+                            t.goods,
+                            t.amount,
+                            t.pay_method,
+                            t.counterparty,
+                            t.category,
+                            t.status,
+                            t.type,
+                            t.source
+                     from transaction t
+                              join account_info a on t.counterparty = a.account_name),
+     transaction_tb as (select t.id,
+                               time,
+                               expenditure_income as exp_income,
+                               goods,
+                               amount,
+                               pay_method,
+                               counterparty,
+                               category,
+                               status,
+                               type,
+                               source
+                        from transaction t
+                                 left join account_info a on t.counterparty = a.account_name
+                        where a.account_name is null
+                        union all
+                        select `id`
+                             , time
+                             , '支出' as exp_income
+                             , goods
+                             , amount
+                             , pay_method
+                             , counterparty
+                             , category
+                             , status
+                             , type
+                             , source
+                        from transfer_tb
+                        union all
+                        select id
+                             , time
+                             , '收入'       as exp_income
+                             , goods
+                             , amount
+                             , counterparty as pay_method
+                             , pay_method   as counterparty
+                             , category
+                             , status
+                             , type
+                             , source
+                        from transfer_tb)
+select id,
+       time,
+       counterparty,
+       goods,
+       amount,
+       sum(case
+               when exp_income = '收入' then amount
+               when exp_income = '支出' then -amount
+               else 0 end) over (partition by pay_method order by time) as balance,
+       pay_method,
+       category,
+       status,
+       type,
+       source
+from transaction_tb
+order by time
+;
+
+
+
+
 # 资产余额表更新
-create view asset_balance as
+create view account__balance as
 select a.id
-     , a.asset_name
-     , a.asset_type
+     , a.account_name
+     , a.account_type
      , t.balance
      , t.income
      , t.expenditure
@@ -114,7 +191,7 @@ select a.id
      , t.update_time
      , a.is_active
      , a.is_included
-from asset_info a
+from account_info a
          left join (select pay_method
                          , min(time)                                       as create_time
                          , max(time)                                       as update_time
@@ -126,20 +203,20 @@ from asset_info a
                          , sum(if(expenditure_income = '支出', amount, 0)) as expenditure
                     from card_transaction
                     group by pay_method) t
-                   on a.asset_name = t.pay_method
-order by asset_type desc, balance desc
+                   on a.account_name = t.pay_method
+order by account_type desc, balance desc
 ;
 
 # 查询资产余额
-select asset_type
+select account_type
      , sum(balance)     as tot_balance
      , sum(income)      as tot_income
      , sum(expenditure) as tot_expenditure
      , max(update_time) as update_time
-from asset_balance
+from account_balance
 where is_active = 1
   and is_included = 1
-group by asset_type;
+group by account_type;
 
 # 月度卡对账单 - 用于校验数据读入
 # TODO: 待完善，若该月无交易，无法拉取各卡数据
@@ -158,19 +235,27 @@ order by month desc, pay_method desc;
 
 
 
-
 # 月度收支对账单 - 用于统计月度收支
 create view monthly_balance as
-select date_format(time, '%Y-%m')                      as month
-     , sum(case
-               when expenditure_income = '收入' then amount
-               when expenditure_income = '支出' then -amount
-               else 0 end)                             as balance
-     , sum(if(expenditure_income = '收入', amount, 0)) as income
-     , sum(if(expenditure_income = '支出', amount, 0)) as expenditure
-from transaction
-group by month
-order by month desc;
+select month,
+       balance,
+       salary,
+       tb.salary - tb.balance as consumption,
+       income,
+       expenditure
+from (select date_format(time, '%Y-%m')                      as month
+           , sum(case
+                     when expenditure_income = '收入' then amount
+                     when expenditure_income = '支出' then -amount
+                     else 0 end)                             as balance
+           , sum(case
+                     when expenditure_income = '收入' and goods rlike '工资|劳务费|讲课费' then amount
+                     else 0 end)                             as salary
+           , sum(if(expenditure_income = '收入', amount, 0)) as income
+           , sum(if(expenditure_income = '支出', amount, 0)) as expenditure
+      from transaction
+      group by month) tb
+order by month;
 
 
 
